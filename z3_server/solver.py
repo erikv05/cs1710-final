ffbbc31fca8aae476ffbf6dfc50bc0881c575804
M-Kron <milokron@gmail.com>
2025-05-03 11:52:13 -0400

    fixed logic bug

--- z3_server/solver.py
+++ z3_server/solver.py
@@ -6,17 +6,13 @@ def solveReq(req: SolverRequest) -> list[SolverResponseEntry]:
     for assertion in solver.assertions:
         print("assertion:")
         print(assertion)
+        print(solver.assertions[assertion])
         solver.s.push()
         solver.s.add(Not(solver.assertions[assertion]))
         result = solver.s.check()
         if result == sat: #if sat, the assertion failed, and should be added to the response entry. otherwise, do nothing
             print("is sat")
             model = solver.s.model()
-            #for var in solver.vars:
-            #    lit = Literal()
-            #    lit.name = var
-            #    lit.assignment = model[solver.vars[var]]
-            #    assignments.append(lit)
             assignments = [Literal(name=var, assignment=bool(model[solver.vars[var]])) for var in solver.vars]
             toReturn.append(SolverResponseEntry(violated_pbt=assertion, transition_used="TODO", violating_state=assignments))
         solver.s.pop()
@@ -40,25 +36,35 @@ class PageTransitionSolver():
         self.assertions = {} # map of pbt_assertions to z3 bools representing whether they are respected
         for transition in self.req.state_transitions:
             self.transitions[transition.name] = Bool(transition.name)
-            for disjunction in transition.lhs + transition.rhs: #transitions are only taken if both rhs and lhs are true
+            for disjunction in transition.lhs + transition.rhs: #transitions can only be taken if both rhs and lhs are true
                 self.s.add(
                     Implies(self.transitions[transition.name], 
                                    Or([self.vars[lit.name] == lit.assignment for lit in disjunction])))
+        
+        
         self.s.add(Or([self.transitions[k] for k in self.transitions]))#self.transitions.values())) #one transtition must be taken
         for assertion in self.req.pbt_assertions:
             self.assertions[assertion.name] = Bool(assertion.name)
             lhs = Bool(assertion.name + '_lhs')
             rhs = Bool(assertion.name + '_rhs')
-            for disjunction in assertion.lhs:
-                print(disjunction)
-                self.s.add(
-                    Implies(lhs, 
-                                   Or([self.vars[lit.name] == lit.assignment for lit in disjunction])))
-            for disjunction in assertion.rhs: 
-                print(disjunction)
-                self.s.add(
-                    Implies(rhs, 
-                                   Or([self.vars[lit.name] == lit.assignment for lit in disjunction])))
+            self.s.add(lhs == And(
+                [Or(
+                    [
+                        self.vars[lit.name] == lit.assignment
+                        for lit in disjunction
+                    ]
+                )
+                    for disjunction in assertion.lhs]
+            ))
+            self.s.add(rhs == And(
+                [Or(
+                    [
+                        self.vars[lit.name] == lit.assignment
+                        for lit in disjunction
+                    ]
+                )
+                    for disjunction in assertion.rhs]
+            ))
             self.s.add(self.assertions[assertion.name] == Implies(lhs, rhs)) #assertion is respected if lhs implies rhs
         
 
