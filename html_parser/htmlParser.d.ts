7a0d58527924ee8e0394f1ffea5ed6857e2d9d94
henreedev <henry_earnest@brown.edu>
2025-05-07 18:55:13 -0400

    add typing for html parser functions

---
+++ html_parser/htmlParser.d.ts
@@ -0,0 +1,14 @@
+export function checkHtmlPropertyValue(
+  htmlString: string,
+  cssClassOrId: string,
+  property: string,
+  value: string
+): boolean;
+
+
+export function checkHtmlPropertyValueByFilepath(
+  htmlFilepath: string,
+  cssClassOrId: string,
+  property: string,
+  value: string
+): boolean;
