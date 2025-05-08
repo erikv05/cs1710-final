2591ec6bfed492d88e22f46d6f3f1f8ff7bac56c
M-Kron <milokron@gmail.com>
2025-05-08 15:46:05 -0400

    more docs

--- z3_server/datatypes.py
+++ z3_server/datatypes.py
@@ -6,28 +6,33 @@ class Literal(BaseModel):
 
 class PbtAssertion(BaseModel): 
     name: str
-    cnf: list[list[Literal]] #If this is satisfiable, the test has failed
+    cnf: list[list[Literal]] #An unsafe condition, represented in Conjunctive normal form.
+                             #If this is satisfiable, the test has failed
 
 class Transition(BaseModel):
-    name: str
-    assignments: list[bool] 
+    name: str #name of the variable in question
+    assignments: list[bool] #possible values this variable can occupy after the transition
 
 class Branch(BaseModel):
-    conditions: list[list[Literal]] #CNF criteria for being in this branch
-    implications: list[Literal] #resulting rendered state
-    transitions: list[Transition]
+    conditions: list[list[Literal]] #CNF criteria for taking this branch
+    implications: list[Literal] #resulting rendered state of this branch
+    transitions: list[Transition] #availible transitions from this branch
 
 class SolverRequest(BaseModel):
-    state_variables: list[str]
-    pbt_variables: list[str]
-    branches: list[Branch]
-    preconditionals: list[list[Literal]]
-    pbt_assertion: PbtAssertion 
+    state_variables: list[str] #state variables being considered
+    pbt_variables: list[str] #variables used for property based testing (these refer to rendered elements)
+    branches: list[Branch] #Use to represent the availbe transitions/rendered states availbe to each
+                           #arrangements of state variables
+    preconditionals: list[list[Literal]] #cnf that must be satisfied by the first state in the trace
+    pbt_assertion: PbtAssertion  #The unsafe condition being tested
 
 class SolverResponse(BaseModel):
     result : str #passed or failed
-    states: list[list[Literal]] #state trace, not a cnf
-    violated_pbt: str #empty if nothing is violated
+    states: list[list[Literal]] #If the test failed, this will be state trace represting the path
+                                #taken to reach the unsafe state. 
+                                #empty if the pass succeeded
+                                #not a cnf
+    violated_pbt: str #empty if the test passed, the name of the assertion if the test failed
 
 
 
